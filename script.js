de5b8e62ade14d4209a09a861891977f3be0c115
Scott Thoreson <scott.thoreson@live.com>
2022-01-13 11:11:40 -0500

    Finished Chapter 26 studio

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+window.addEventListener("load",function(){
+    const fetchPromise = this.fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+    fetchPromise.then(function(response){
+        
+        response.json().then( function(json){
+            let div = document.getElementById("container");
+            let index = 0;
+            for(index = 0; index<json.length;index++){
+            div.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[index].hoursInSpace}</li>
+                            <li>Active: ${json[index].active}</li>
+                            <li>Skills: ${json[index].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[index].picture}">
+                </div>
+            `;
+            // index = (index + 1) % json.length;
+            }
+        });
+        // }
+    });
+});
